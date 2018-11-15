25c2abff6c44e72f1597af14c9ae13fd68772618
Moagan Rangan D <moganvig90@gmail.com>
2018-11-15 22:22:51 +0530

    Enabled product list click

--- f2h.web/src/app/app-routing.module.ts
+++ f2h.web/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'product-list', component: ProductListComponent },
   { path: '**', redirectTo: '' },
 ];
 
