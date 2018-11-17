6dd38ffcaec49efa67c2f5068743186734d4fab7
Moagan Rangan D <moganvig90@gmail.com>
2018-11-17 22:02:27 +0530

    Product details

--- f2h.web/src/app/app-routing.module.ts
+++ f2h.web/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product-list', component: ProductListComponent },
+  { path: 'product-detail', component: ProductDetailComponent },
   { path: '**', redirectTo: '' },
 ];
 
