4045e7ce12f6164b3b2bab763f3caa92a26d6cc9
Moagan Rangan D <moganvig90@gmail.com>
2018-11-25 21:25:27 +0530

    Template update for product details.

--- f2h.web/src/app/app-routing.module.ts
+++ f2h.web/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ViewcartComponent } from './viewcart/viewcart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product-list', component: ProductListComponent },
   { path: 'product-detail', component: ProductDetailComponent },
+  { path: 'view-cart', component: ViewcartComponent },
   { path: '**', redirectTo: '' },
 ];
 
